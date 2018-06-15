f651b561539c8581fb7dccada66df7541dabb80d
Aayu <rajusah.aayu@gmail.com>
2018-06-15 17:42:29 +0530

    Initial commit

--- app/src/main/java/bugsolvers/bms/com/sequenceorderview/AddViewActivity.java
+++ app/src/main/java/bugsolvers/bms/com/sequenceorderview/AddViewActivity.java
@@ -19,6 +19,9 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+    Raju Sah
+* */
 public class AddViewActivity extends AppCompatActivity
 {
     private GridView gridView;
@@ -34,13 +37,14 @@ public class AddViewActivity extends AppCompatActivity
     private View genericView;
     private List<String> stringList;
     ArrayAdapter<String> adapterd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        gridView = (GridView) findViewById(R.id.grid);
+        gridView = findViewById(R.id.grid);
         btnGo = findViewById(R.id.button);
         lvDynamic = findViewById(R.id.lvDynamic);
         stringList = new ArrayList<>();
@@ -64,7 +68,7 @@ public class AddViewActivity extends AppCompatActivity
                     adapter.selectedPositions.remove(selectedIndex);
                     ((GridItemView) v).display(false);
 
-                    selectedStrings.remove((String) parent.getItemAtPosition(position));
+                    selectedStrings.remove(parent.getItemAtPosition(position));
 
                     //((LinearLayout) genericView.getParent()).removeView(genericView);
 
@@ -79,7 +83,7 @@ public class AddViewActivity extends AppCompatActivity
                     selectedStrings.add((String) parent.getItemAtPosition(position));
                     stringList.add(selectedItem);
 
-                     adapterd = new ArrayAdapter<String>(AddViewActivity.this, R.layout.sequence_row, R.id.tvItemSelected, stringList);
+                    adapterd = new ArrayAdapter<String>(AddViewActivity.this, R.layout.sequence_row, R.id.tvItemSelected, stringList);
 
                     // Assign adapter to ListView
                     lvDynamic.setAdapter(adapterd);
@@ -136,13 +140,13 @@ public class AddViewActivity extends AppCompatActivity
         assert layoutInflater != null;
         final View addView = layoutInflater.inflate(R.layout.sequence_row, null);
         genericView = addView;
-        final TextView tvStripe = (TextView) addView.findViewById(R.id.tvStripe);
-        final TextView tvItemSelected = (TextView) addView.findViewById(R.id.tvItemSelected);
+        final TextView tvStripe = addView.findViewById(R.id.tvStripe);
+        final TextView tvItemSelected = addView.findViewById(R.id.tvItemSelected);
         tvItemSelected.setText(selectedItem);
         // tvStripe.setText(textIn.getText().toString());
-        ImageView ivDecrease = (ImageView) addView.findViewById(R.id.ivDecrease);
-        ImageView ivIncrease = (ImageView) addView.findViewById(R.id.ivIncrease);
-        final ImageView ivRemove = (ImageView) addView.findViewById(R.id.ivRemove);
+        ImageView ivDecrease = addView.findViewById(R.id.ivDecrease);
+        ImageView ivIncrease = addView.findViewById(R.id.ivIncrease);
+        final ImageView ivRemove = addView.findViewById(R.id.ivRemove);
         ivDecrease.setOnClickListener(new View.OnClickListener()
         {
             @Override
